c72cf07d7095ff46e2b1f737d2cd5f39f4cc1c00
vandosant <sjskender@gmail.com>
2014-08-08 10:19:55 -0600

    Added unit tests for pairing dates

---
+++ spec/models/pairing.rb
@@ -0,0 +1,14 @@
+describe Pairing do
+  it "is invalid without a pair date" do
+    pairing = Pairing.new(:pair_date => nil)
+    expect(pairing).to_not be_valid
+    expect(pairing).to have(1).error_on :pair_date
+  end
+
+  it "is valid with a pair date" do
+    pairing = Pairing.new(:pair_date => 1.day.ago)
+    expect(pairing).to be_valid
+    expect(pairing).to have(0).errors_on :pair_date
+  end
+
+end
