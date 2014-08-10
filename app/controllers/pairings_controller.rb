cd52123fe18c80d6a090d4187804cb67711afac6
vandosant <sjskender@gmail.com>
2014-08-10 11:26:06 -0600

    Refactoring
    * Using nested routes more explicitly

--- app/controllers/pairings_controller.rb
+++ app/controllers/pairings_controller.rb
@@ -5,14 +5,14 @@ class PairingsController < ApplicationController
   end
 
   def create
+    @pair = User.find(params[:user_id])
     @pairing = Pairing.new(pairing_params.merge({
-                                                  user_id: params[:user_id],
-                                                  pair_id: current_user.id
+                                                  user_id: current_user.id,
+                                                  pair_id: @pair.id
                                                 }))
     if @pairing.save
       redirect_to root_path
     else
-      @pair = User.find(params[:user_id])
       render :new
     end
   end
